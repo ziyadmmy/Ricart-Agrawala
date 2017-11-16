df41cf3c239d86036859023c92eaee06c1b26c5e
MechCoder0 <30991841+MechCoder0@users.noreply.github.com>
2017-11-16 16:06:50 -0500

    Update ThreadTesting.java

--- ThreadTesting.java
+++ ThreadTesting.java
@@ -1,13 +1,31 @@
+package distributedSystems;
+
+import java.util.Scanner;
+
 public class ThreadTesting {
 
-	   public static void main(String args[]) {
-	      Threading R1 = new Threading("Thread-1");
-	      R1.start();
-	      
-	      Threading R2 = new Threading("Thread-2");
-	      R2.start();
-	      
-	      Threading R3 = new Threading("Thread-3");
-	      R3.start();
-	   }   
+	public static void main(String args[]) {
+		String input;
+		int number;
+		boolean flag = false;
+		Threading thread;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Hello! Enter the number of threads you would like to start");
+
+		do {
+			input = scan.next();
+			try {
+				number = Integer.parseInt(input);
+				for (int i = 1; i <= number; i++) {
+					thread = new Threading("Thread-" + i);
+					thread.start();
+					}
+			} catch (Exception e) {
+				System.out.println("Please enter a number");
+				flag = true;
+			}
+		} while (flag);
+		scan.close();
 	}
+}
