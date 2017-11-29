e881afa41a4ff4fd04bec00592c4058c5b875f39
MechCoder0 <30991841+MechCoder0@users.noreply.github.com>
2017-11-29 15:52:18 -0500

    Update Threading.java

--- Threading.java
+++ Threading.java
@@ -1,55 +1,69 @@
 package distributedSystems;
 
 class Threading implements Runnable {
-	   private Thread t;
-	   private String threadName;
-	   private static int count=0;
-	   private int num;
-	   private int runTime;
-	   private Processing test = new Processing();
-	   
-	   Threading (String name) {
-	      threadName = name;
-	      System.out.println("Creating " +  threadName);
-	      count ++;
-	      num = count-1;
-	      runTime = count *10;
-	      test.addLink();
-	      test.Print();
-	   }
-	   
-	   public void run() {
-	      System.out.println("Running " +  threadName);
-	      try {
-	         for(int i = runTime; i >0; i--) {
-	            System.out.println("Thread: " + threadName + ", " + i);
-	            Thread.sleep(50);
-	            //check for requests
-	            if(test.checkRequest()){
-	            	test.sendReply(num);
-	            	//test this, also implement a queue using timestamps?
-	            }
-	         }
-	      } catch (Exception e) {
-	         System.out.println("Thread " +  threadName + " interrupted.");
-	      }
-	      test.sendRequest(num);
-	      System.out.println("Thread " +  threadName + " exiting.");
-	   }
-	   
-	   public void start () {
-	      System.out.println("Starting " +  threadName );
-	      if (t == null) {
-	         t = new Thread (this, threadName);
-	         t.start ();
-	         }
-	   }
-	   
-	   public void enterCS(){
-		   //Checks for all replies
-		   //Thread enters the critical section, 
-		   //reads the data, 
-		   //waits 50 ms Thread.sleep(50);
-		   //exits
-	   }
+	private Thread t;
+	private String threadName;
+	private static int count = 0;
+	private int num;
+	private int runTime;
+	private Processing test = new Processing();
+
+	Threading(String name) {
+		threadName = name;
+		System.out.println("Creating " + threadName);
+		count++;
+		num = count - 1;
+		runTime = count * 10;
+		test.addLink();
+		test.Print();
 	}
+
+	public void run() {
+		System.out.println("Running " + threadName);
+		try {
+			for (int i = runTime; i > 0; i--) {
+				System.out.println("Thread: " + threadName + ", " + i);
+				Thread.sleep(50);
+				// check for requests
+				if (test.checkRequest()) {
+					test.sendReply(num);
+					// test this, also implement a queue using timestamps?
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Thread " + threadName + " interrupted.");
+		}
+		test.sendRequest(num);
+		test.enque(num);
+		while (!test.checkReplys()) {
+			if (test.checkRequest()) {
+				test.sendReply(num);
+				// test this, also implement a queue using timestamps?
+			}
+		}
+		while(!test.checkFirst(num)){
+			System.out.println("waiting");
+		}
+		enterCS();
+		test.deque();
+		System.out.println("Thread " + threadName + " exiting.");
+	}
+
+	public void start() {
+		System.out.println("Starting " + threadName);
+		if (t == null) {
+			t = new Thread(this, threadName);
+			t.start();
+		}
+	}
+
+	public void enterCS() {
+		try {
+			System.out.println("Thread " + threadName + " is entering the critical section");
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
