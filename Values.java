2f176f7925079f696fa752b241fd93b8c33dfe74
MechCoder0 <30991841+MechCoder0@users.noreply.github.com>
2017-12-02 13:35:36 -0500

    Update Values.java

--- Values.java
+++ Values.java
@@ -1,33 +1,22 @@
 package distributedSystems;
 
-import java.util.Date;
-
+//This is the object that we will be passing around
 public class Values {
-	private boolean request=false,reply=false;
-	private Date date;
-	
-	public void setReply(boolean reply){
+	private boolean request = false, reply = false;
+
+	public void setReply(boolean reply) {
 		this.reply = reply;
 	}
-	
-	public boolean getReply(){
+
+	public boolean getReply() {
 		return reply;
 	}
-	
-	public void setRequest(boolean request){
+
+	public void setRequest(boolean request) {
 		this.request = request;
 	}
-	
-	public boolean getRequest(){
+
+	public boolean getRequest() {
 		return request;
 	}
-	
-	public void setDate(){
-		date = new Date();
-	}
-	
-	public Date getDate(){
-		return date;
-	}
-
 }
